0670f5541e4bae550238ffd6a700aa2158374a38
Denis Konev <deniskonev1984@gmail.com>
2024-06-14 22:37:19 +0300

    2_6_dependency_injection
    Signed-off-by: Denis Konev <deniskonev1984@gmail.com>

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -5,9 +5,16 @@ import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.List;
 
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
 public class UserService {
 
-    private UserRepository repository;
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
 
     public User create(User user) {
         return repository.save(user);
